6d50ec88c881a9198cadf9cdf2e39471cb1c4777
Ishant Kumar <er.ishant@gmail.com>
2019-03-12 12:37:13 +0530

    Session11 - Android MediaPlayer and Fragments

---
+++ app/src/main/java/com/auribises/enc2019a/LowerFragment.java
@@ -0,0 +1,47 @@
+package com.auribises.enc2019a;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LowerFragment extends Fragment {
+
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+    public LowerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_lower, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+
+        adapter.add("NDTV");        //0
+        adapter.add("CNN");
+        adapter.add("ZEE");
+        adapter.add("AAJ TAK");
+        adapter.add("PTC");         //n-1
+
+        listView.setAdapter(adapter);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/auribises/enc2019a/MyFragmentsActivity.java
@@ -0,0 +1,34 @@
+package com.auribises.enc2019a;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFragmentsActivity extends AppCompatActivity {
+
+
+    UpperFragment upperFragment;
+    LowerFragment lowerFragment;
+    FragmentManager fragmentManager;
+
+    void initViews(){
+
+        upperFragment = new UpperFragment();
+        lowerFragment = new LowerFragment();
+
+        // FragmentManager will help to manage fragments in Activity
+        // eg: to add/delete/update fragment in Activity
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.upperFrame,upperFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.lowerFrame,lowerFragment).commit();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_fragments);
+        initViews();
+    }
+}
