d9675bc314bf3bfb161dcba62a9c7aeb65531293
Ishant Kumar <er.ishant@gmail.com>
2019-03-22 12:08:13 +0530

    Session16 - Android LocationManager and GoogleMaps

--- app/src/main/java/com/auribises/enc2019a/SplashActivity.java
+++ app/src/main/java/com/auribises/enc2019a/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             if(msg.what == 101){
-                Intent intent = new Intent(SplashActivity.this, BooksActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MapsActivity.class);
                 startActivity(intent);
                 finish();
             }
