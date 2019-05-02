5f8648156efa4023bf81f03efe86d120dd67b416
Ishant Kumar <er.ishant@gmail.com>
2019-05-02 11:39:43 +0530

    Cloud Functions

---
+++ app/src/main/res/raw/index.js
@@ -0,0 +1,57 @@
+// Cloud Functions in Firebase
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	 
+	// Write Javascript Code to return current date and time
+	//var d = new Date();
+	//response.send("This is Hello from Auribises !!"+d);
+
+	// Fetch Data from Firebase Firestore and Convert it to 
+	var db = admin.firestore();
+	//var usersRef = db.collection('users').orderBy('name','desc');
+	var usersRef = db.collection('users');
+	var users = [];
+	var allUsers = usersRef.get()
+	.then(snapshot => {
+	    snapshot.forEach(doc => {
+	      console.log(doc.id, '=>', doc.data());
+	      users.push(doc.data())
+	    });
+	    var jsonData = JSON.stringify(users);
+	    response.send(jsonData);
+  		return;
+  	})
+	.catch(err => {
+	    console.log("Error getting documents"+err);
+	});
+
+});
+
+exports.fetchUser = functions.https.onRequest((request, response) => {
+	 
+    const name = request.query.name;
+
+	var db = admin.firestore();
+	var usersRef = db.collection('users').where('name', '==', name);
+	var users = [];
+	var allUsers = usersRef.get()
+	.then(snapshot => {
+	    snapshot.forEach(doc => {
+	      console.log(doc.id, '=>', doc.data());
+	      users.push(doc.data())
+	    });
+	    var jsonData = JSON.stringify(users);
+	    response.send(jsonData);
+  		return;
+  	})
+	.catch(err => {
+	    console.log("Error getting documents"+err);
+	});
+
+});
